a40f3ba532eb72b5d6a239ee17c32147cc0b262f
Michel <michelmukua@DESKTOP-I2DGMUQ.localdomain>
2025-03-14 16:07:25 -0600

    Create project structure

---
+++ src/com/skilldistillery/animals/app/AnimalSanctuaryApp.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.animals.app;
+
+//has a scanner
+public class AnimalSanctuaryApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
